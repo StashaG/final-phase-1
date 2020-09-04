8bc4c1ed5ea22984202afe0dd936dafae4d8b74c
Robert <r.powelliv@gmail.com>
2020-09-03 20:02:35 -0400

    Rob

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(()=> {
   $('form').on("submit", function(e){
     e.preventDefault(); 
 
-      let recipe = fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${$('.search-bar').val()}&number=2&apiKey=45f77eeec63346cdb3c6db8bfd6183e3`)
+      let recipe = fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${$('.search-bar').val()}&number=10&apiKey=45f77eeec63346cdb3c6db8bfd6183e3`)
           .then(response => response.json())
           .then(data => {
             console.log(data);
@@ -17,13 +17,14 @@ $(()=> {
                     <h5 class="card-title" id="recipeName">${data.title}</h5>
                     <img class="card-img-top"id="image" src="${data.image}" alt="Card image cap">
                     <div class="card-body">
-                      <p class="card-text" id="recipe">${data.usedIngredients}</p>
+                      <p class="card-text" id="recipe">${data.id}</p>
                     </div>`
                 });
                 $('.recipeOp').html(recipeFind.join(' '))
+                // $('.recipeOp').append(recipeFind)
       
         };
-        recipesuggestion(data.Search);
+        recipesuggestion(data);
       
       });
 
@@ -32,23 +33,7 @@ $(()=> {
 
   });
 
-            // console.log(data);
-            // function renderMovies(movieArray) {
-            //     console.log("I'm here")
-            //     // console.log(movieArray)
-        
-            //     finalHtml = movieArray.map(data => {
-            //         return `<div class="card" style="width: 18rem;">
-            //         <img class="card-img-top"id="image" src="${movie.Poster}" alt="Card image cap">
-            //         <div class="card-body">
-            //           <h5 class="card-title" id="title">${movie.Title}</h5>
-            //           <p class="card-text" id="release-date">${movie.Year}</p>
-            //           <a href="#" onclick="saveToWatchlist('${movie.imdbID}')"class="btn btn-primary" id="imdb">ADD</a>
-            //         </div>`
-            //     });
-            //     $('.results').html(finalHtml.join(' '))
-            // }
-        
+
               
               
         //     $('form').on("submit", function(e){
