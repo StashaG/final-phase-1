db57a4061253c3b464af8afd2223e883d8a5446c
Robert <r.powelliv@gmail.com>
2020-09-04 21:01:12 -0400

    Fetch issue

--- index.js
+++ index.js
@@ -1,41 +1,69 @@
 $(()=> {
-  console.log("Ready")
-
   $('form').on("submit", function(e){
-    e.preventDefault(); 
-    console.log('start')
-
-
+    e.preventDefault();
     async function getFood () {
       let data = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${$('.search-bar').val()}&number=10&apiKey=45f77eeec63346cdb3c6db8bfd6183e3`)
-      let dataOne = await data.json()
-      let recipe = await fetch(`https://api.spoonacular.com/recipes/${dataOne.id}/summary?apiKey=45f77eeec63346cdb3c6db8bfd6183e3`)
-      
+      let items = await data.json();
+      renderItems(items);
+    }
+    function renderItems(items) {
+      items.forEach((item, i) => {
+        fetch(`https://api.spoonacular.com/recipes/${item.id}/summary?apiKey=45f77eeec63346cdb3c6db8bfd6183e3`)
+          .then(data => data.json()).then(recipe => renderItem(recipe));
+      });
+    }
+    function renderItem(item) {
+      const recipe = `
+        <div class="card" style="width: 18rem;">
+          <h5 class="card-title" id="recipeName">${item.title}</h5>
+          <img class="card-img-top"id="image" src="${item.image}" alt="Card image cap" />
+          <div class="card-body">
+            <p class="card-text" id="recipe">${item.summary}</p>
+          </div>
+        </div>
+      `;
+      $('#recipeOp').append(recipe);
+    }
+    getFood();
+  });
+});
+// $(()=> {
+//   console.log("Ready")
+
+//   $('form').on("submit", function(e){
+//     e.preventDefault(); 
+//     console.log('start')
+
+
+//     async function getFood () {
+//       let data = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${$('.search-bar').val()}&number=10&apiKey=45f77eeec63346cdb3c6db8bfd6183e3`)
+//       let dataOne = await data.json()
+//       let recipe = await fetch(`https://api.spoonacular.com/recipes/${dataOne.id}/summary?apiKey=45f77eeec63346cdb3c6db8bfd6183e3`)
 
   
-            console.log(data);
-            function recipeSuggestion(fridgeFood) {
-                console.log("I'm here")
-                console.log(fridgeFood)
+//             console.log(data);
+//             function recipeSuggestion(fridgeFood) {
+//                 console.log("I'm here")
+//                 console.log(fridgeFood)
         
-                recipeFind = fridgeFood.map(data => {
-                    return `<div class="card" style="width: 18rem;">
-                    <h5 class="card-title" id="recipeName">${data.title}</h5>
-                    <img class="card-img-top"id="image" src="${data.image}" alt="Card image cap">
-                    <div class="card-body">
-                      <p class="card-text" id="recipe">${recipe.summary}</p>
-                    </div>
-                    </div>`
+//                 recipeFind = fridgeFood.map(data => {
+//                     return `<div class="card" style="width: 18rem;">
+//                     <h5 class="card-title" id="recipeName">${data.title}</h5>
+//                     <img class="card-img-top"id="image" src="${data.image}" alt="Card image cap">
+//                     <div class="card-body">
+//                       <p class="card-text" id="recipe">${recipe.summary}</p>
+//                     </div>
+//                     </div>`
 
-                });
+//                 });
 
-                $('.recipeOp').html(recipeFind.join(' '));
-        };
-        recipeSuggestion(data);
-      };
-      getFood();
-    });
-  });
+//                 $('.recipeOp').html(recipeFind.join(' '));
+//         };
+//         recipeSuggestion(data);
+//       };
+//       getFood();
+//     });
+//   });
 
 
               
