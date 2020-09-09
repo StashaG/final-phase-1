1857ef4108022656cc3f03845fa4add884e0a04a
Robert <r.powelliv@gmail.com>
2020-09-08 20:43:58 -0400

    Populate

--- index.js
+++ index.js
@@ -3,18 +3,11 @@ $(()=> {
     e.preventDefault();
     async function getFood () {
       let data = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${$('.search-bar').val()}&number=20&apiKey=${apiKey}`)
-      console.log(data);
+      // console.log(data);
       let items = await data.json();
       foodID(items);
     }
     function foodID(items) {
-      // items.forEach((item, i) => {
-      //   fetch(`https://api.spoonacular.com/recipes/${item.id}/summary?apiKey=${apiKey}`)
-      //     .then(data => data.json())
-      //     .then(recipe => 
-      //       getRecipe(recipe, item.image));
-          
-      // });
       items.forEach((item, i) => {
           fetch(`https://api.spoonacular.com/recipes/${item.id}/information?apiKey=${apiKey}`)
             .then(data => data.json()).then(recipe => getRecipe(recipe, item.image));
@@ -24,14 +17,14 @@ $(()=> {
 
 
     function getRecipe(item, image) {
-      console.log(item);
-      sourceU = item.sourceUrl;
-      console.log(sourceU);
+      // console.log(item);
+      // sourceU = item.sourceUrl;
+      // console.log(sourceU);
 
-        let instruct = fetch(`https://api.spoonacular.com/recipes/extract?url=${sourceU}&apiKey=${apiKey}`)
-            .then(data => data.json());
-            console.log('fetch')
-            console.log(instruct)
+      //   let instruct = fetch(`https://api.spoonacular.com/recipes/extract?url=${sourceU}&apiKey=${apiKey}`)
+      //       .then(data => data.json());
+      //       console.log('fetch')
+      //       console.log(instruct)
           
     
       const recipe = `
@@ -42,18 +35,50 @@ $(()=> {
             <p class="card-text" id="instruct"></p>
             <p class="card-text" id="recipe">${item.summary}</p>
           </div>
-          <button type="button" class="btn btn-warning pickMe">PICK THIS RECIPE</button>
+          <button id="mealPick" type="button" class="btn btn-warning">PICK THIS RECIPE</button>
         </div>
       `;
       $('#recipeOp').append(recipe);
+      //
+      $('#mealPick').click(function() {
+        console.log('New Card');
+        $('#recipeOp').empty();
+        const recipeChoice = `
+        <div class="card" style="width: 18rem;">
+          <h5 class="card-title" id="recipeName">${item.id}</h5>
+          <img class="card-img-top"id="image" src="${image}" alt="Card image cap" />
+          <div class="card-body">
+            <p class="card-text" id="instruct"></p>
+            <p class="card-text" id="recipe">${item.sourceUrl}</p>
+          </div>
+          <button id="mealPick" type="button" class="btn btn-warning">PICK THIS RECIPE</button>
+        </div>`;
+        console.log(recipeChoice)
+        $('#recipeOp').append(recipeChoice);
+      });
     }
     $('#recipeOp').empty();
+    console.log('empty')
     getFood();
     console.log('Here')
   });
   
 });
 
+// $('#mealPick').click(function() {
+//   console.log('New Card');
+//   // $('#recipeOp').empty();
+//   // const recipe = `
+//   // <div class="card" style="width: 18rem;">
+//   //   <h5 class="card-title" id="recipeName">${item.title}</h5>
+//   //   <img class="card-img-top"id="image" src="${image}" alt="Card image cap" />
+//   //   <div class="card-body">
+//   //     <p class="card-text" id="instruct"></p>
+//   //     <p class="card-text" id="recipe">${item.summary}</p>
+//   //   </div>
+//   //   <button id="mealPick" type="button" class="btn btn-warning">PICK THIS RECIPE</button>
+//   // </div>`;
+// });
 
 
 
