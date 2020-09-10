263d21ef60eb89ea4fc9458878d574b7fb9c23b3
Stacey Graham <stacey.graham1106@gmail.com>
2020-09-10 18:29:27 -0400

    adding auto complete feature

--- index.js
+++ index.js
@@ -1,12 +1,83 @@
+let items = [];
 $(()=> {
-  $('form').on("submit", function(e){
-    e.preventDefault();
-    async function getFood () {
-      let data = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${$('.search-bar').val()}&number=20&apiKey=${apiKey}`)
-      // console.log(data);
-      let items = await data.json();
-      foodID(items);
-    }
+
+  // const endpoint = "https://api.spoonacular.com/recipes/autocomplete?number=10&query=${$('.search-bar').val()}&apiKey=${apiKey3}";
+  
+  
+  $(".search-bar").on('input', function() {
+    console.log($('.search-bar').val());
+    let endpoint = `https://api.spoonacular.com/recipes/autocomplete?number=10&query=${$('.search-bar').val()}&apiKey=218d75a1a0d54dc1afef8d7d84763a57`;
+    console.log(endpoint);
+    fetch(endpoint)
+      .then(blob => blob.json())
+      .then(data => {
+        console.log(data);
+        items.push(...data)
+        });  
+    console.log(items);
+  
+  });
+  function findMatches(wordToMatch, items) {
+    return items.filter(foodItem => {
+    // here we need to figure out if the item is a match for what was searched
+    const regex = new RegExp(wordToMatch, 'gi');
+    return foodItem.title.match(regex)
+  });
+}
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+function displayMatches() {
+  const matchArray = findMatches(this.value, items);
+  const html = matchArray.map(place => {
+  const regex = new RegExp(this.value, 'gi');
+  const newFoodItem = place.title.replace(regex, `<span class="hl">${this.value}</span>`);
+return `
+  <li>
+    <span class="name">${newFoodItem}</span>
+  </li>
+`;
+}).join('');
+suggestions.innerHTML = html;
+}
+$('.hl').click(function(){
+  console.log('hello');
+  console.log($(this).text())
+  fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${$(this).text()}&number=2`)
+  .then(blob => blob.json())
+  .then(data => {
+    console.log(data);
+    });  
+})
+const searchInput = document.querySelector('.search-bar');
+const suggestions = document.querySelector('.suggestions');
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
+
+
+  // $('form').on("submit", function(e){
+  //   e.preventDefault();
+  //   async function getFood () {
+  //     let data = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${$('.search-bar').val()}&number=20&apiKey=${apiKey}`)
+  //     // console.log(data);
+  //     let items = await data.json();
+  //     foodID(items);
+  //   }
+
+    //Autocomplete feature
+
+
+    $('form').on("submit", function(e){
+      console.log($('.search-bar').val());
+      e.preventDefault();
+      async function getFood () {
+        let data = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${$('.search-bar').val()}&number=20&apiKey=${apiKey}`)
+        // console.log(data);
+        let items = await data.json();
+        foodID(items);
+      }
+
+      
 
     function foodID(items) {
       items.forEach((item, i) => {
