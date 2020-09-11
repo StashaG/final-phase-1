c3e2b27ff59653e71509874519d99fc1a436c0b4
bensvarnum <bens.varnum@gmail.com>
2020-09-10 21:04:55 -0400

    resolve merge conflict

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
 $(() => {
   $("form").on("submit", function (e) {
     e.preventDefault();
-
+    loading();
     async function getFood() {
       let data = await fetch(
         `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${$(
           ".search-bar"
-        ).val()}&number=10&apiKey=${apiKey}`
+        ).val()}&number=20&apiKey=${apiKey}`
       );
-
-    async function getFood () {
-      let data = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${$('.search-bar').val()}&number=20&apiKey=${apiKey}`)
-      // console.log(data);
-
-    loading();
-    async function getFood () {
-      let data = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${$('.search-bar').val()}&number=20&apiKey=${apiKey}`)
       console.log(data);
-
       let items = await data.json();
       removeLoading();
       foodID(items);
-      
     }
 
     function foodID(items) {
@@ -33,28 +23,24 @@ $(() => {
           .then((recipe) => getRecipe(recipe, item.image));
       });
     }
-<<<<<<< HEAD
+
     function getRecipe(item, image) {
-=======
-    
-    function getRecipe(item, image) { 
       let details = JSON.stringify(item.extendedIngredients);
       let analyze = JSON.stringify(item.analyzedInstructions);
-      
+
       //Pull name
 
       // var getIng = details.map(function (ingNew) {
-      //   return ingNew.name;  
+      //   return ingNew.name;
       // });
-      
-      // ES6
-      let getIng = details.map(ingNew => {
-        return ingNew.name;
-      });
-      
-      console.log(getIng);
 
->>>>>>> development
+      // ES6
+      //let getIng = details.map((ingNew) => {
+      //return ingNew.name;
+      // });
+
+      //console.log(getIng);
+
       const recipe = `
         <div class="card" style="width: 18rem;">
           <h5 class="card-title" id="recipeName">${item.title}</h5>
@@ -65,45 +51,17 @@ $(() => {
             <p class="card-text" id="recipe">${details}</p>
             <p class="card-text" id="recipe">${analyze}</p>
           </div>
-<<<<<<< HEAD
-        </div>
-      `;
-
-      console.log("Check");
+        </div>`;
+      console.log(recipe);
       $("#recipeOp").append(recipe);
-<<<<<<< HEAD
 
       //MAKES RECIPE CHOICE DIV
 
-      $("#mealPick").click(function () {
+      $("#mealPick").one("click", function () {
         console.log("New Card");
 
         $("#recipeOp").empty();
-        console.log(item);
-        // sourceU = item.sourceUrl;
-        // console.log(sourceU);
 
-        //   let instruct = fetch(`https://api.spoonacular.com/recipes/extract?url=${sourceU}&apiKey=${apiKey}`)
-        //       .then(data => data.json());
-        //       console.log('fetch')
-        //       console.log(instruct)
-=======
-=======
-=======
-        </div>`
-        console.log(recipe);
-      ;
->>>>>>> dc778cf4eb55c71ef31acf41af82c72afac29306
-      $('#recipeOp').append(recipe);
-
-      //MAKES RECIPE CHOICE DIV
-
-      $('#mealPick').one("click", function() {
-        console.log('New Card');
-
-        $('#recipeOp').empty();
-          
->>>>>>> development
         const recipeChoice = `
         <div class="card" style="width: 18rem;">
           <h5 class="card-title" id="recipeName">${item.title}</h5>
@@ -117,127 +75,21 @@ $(() => {
         $("#recipeOp").append(recipeChoice);
       });
     }
-    $('#recipeOp').empty();
-    
+    $("#recipeOp").empty();
+
     getFood();
     console.log("Here");
   });
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> development
 });
 
-// $(()=> {
-//   console.log("Ready")
-
-//   $('form').on("submit", function(e){
-//     e.preventDefault();
-//     console.log('start')
-
-//     async function getFood () {
-//       let data = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${$('.search-bar').val()}&number=10&apiKey=45f77eeec63346cdb3c6db8bfd6183e3`)
-//       let dataOne = await data.json()
-//       let recipe = await fetch(`https://api.spoonacular.com/recipes/${dataOne.id}/summary?apiKey=45f77eeec63346cdb3c6db8bfd6183e3`)
-
-//             console.log(data);
-//             function recipeSuggestion(fridgeFood) {
-//                 console.log("I'm here")
-//                 console.log(fridgeFood)
-
-//                 recipeFind = fridgeFood.map(data => {
-//                     return `<div class="card" style="width: 18rem;">
-//                     <h5 class="card-title" id="recipeName">${data.title}</h5>
-//                     <img class="card-img-top"id="image" src="${data.image}" alt="Card image cap">
-//                     <div class="card-body">
-//                       <p class="card-text" id="recipe">${recipe.summary}</p>
-//                     </div>
-//                     </div>`
-
-<<<<<<< HEAD
-//});
-=======
-
-
-
-
-  
-=======
->>>>>>> dc778cf4eb55c71ef31acf41af82c72afac29306
-  
-});
->>>>>>> development
-
-function loading (){
-  console.log("loading")
-  $(".container").append("<div class = 'loading'><img src = 'images/loading.gif'></div>")
-} 
-
-function removeLoading () {
-  console.log("loading removed")
-  $(".loading").remove();
+function loading() {
+  console.log("loading");
+  $(".container").append(
+    "<div class = 'loading'><img src = 'images/loading.gif'></div>"
+  );
 }
 
-// $('#mealPick').click(function() {
-//   console.log('New Card');
-//   // $('#recipeOp').empty();
-//   // const recipe = `
-//   // <div class="card" style="width: 18rem;">
-//   //   <h5 class="card-title" id="recipeName">${item.title}</h5>
-//   //   <img class="card-img-top"id="image" src="${image}" alt="Card image cap" />
-//   //   <div class="card-body">
-//   //     <p class="card-text" id="instruct"></p>
-//   //     <p class="card-text" id="recipe">${item.summary}</p>
-//   //   </div>
-//   //   <button id="mealPick" type="button" class="btn btn-warning">PICK THIS RECIPE</button>
-//   // </div>`;
-// });
-
-//     $('form').on("submit", function(e){
-//       e.preventDefault();
-//       let searchString = $('.search-bar').val();
-//       urlEncodedSearchString = encodeURIComponent(searchString);
-//       console.log(urlEncodedSearchString);
-
-//             renderMovies(data.Search);
-//           });
-//         console.log(newMovie);
-//         console.log('Is it broken');
-
-//       });
-
-//   });
-
-<<<<<<< HEAD
-//     function ButtonClicked() {
-//         $('#formsubmitbutton').hide();
-//         $('#formElem').hide();
-//         $('#buttonreplacement').show();
-//         $('#buttonreplacement').append(
-//             `<h2>YOUR HERO</h2>
-//             <img src="img/sVen.jpg" alt="loading...">
-//             <p>Weapon Choice: ${$('#weaponChoiceH option:selected').text()}</p>
-//             <h1>VS.</h1>
-//             <h2>YOUR VILLIAN</h2>
-//             <img src="img/dragon-legendary-creature-sea-serpent-art-creatures-png-clip-art.png" alt="loading...">
-//             <p>Weapon Choice: ${$('#weaponChoiceV option:selected').text()}</p>`)
-
-//     };
-=======
-      
-        //     function ButtonClicked() {
-        //         $('#formsubmitbutton').hide();
-        //         $('#formElem').hide();
-        //         $('#buttonreplacement').show(); 
-        //         $('#buttonreplacement').append(
-        //             `<h2>YOUR HERO</h2>
-        //             <img src="img/sVen.jpg" alt="loading...">
-        //             <p>Weapon Choice: ${$('#weaponChoiceH option:selected').text()}</p>
-        //             <h1>VS.</h1>
-        //             <h2>YOUR VILLIAN</h2>
-        //             <img src="img/dragon-legendary-creature-sea-serpent-art-creatures-png-clip-art.png" alt="loading...">
-        //             <p>Weapon Choice: ${$('#weaponChoiceV option:selected').text()}</p>`)
-        
-        //     };
->>>>>>> dc778cf4eb55c71ef31acf41af82c72afac29306
+function removeLoading() {
+  console.log("loading removed");
+  $(".loading").remove();
+}
