6ab40d72a5992c112f901e5074d9eda0fd57b25e
Robert <r.powelliv@gmail.com>
2020-09-10 21:27:52 -0400

    New Card

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ $(()=> {
   $('form').on("submit", function(e){
     e.preventDefault();
     loading();
+    
     async function getFood () {
       let data = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${$('.search-bar').val()}&number=20&apiKey=${apiKey}`)
-      console.log(data);
+      // console.log(data);
       let items = await data.json();
       removeLoading();
       foodID(items);
@@ -21,63 +22,50 @@ $(()=> {
     
     function getRecipe(item, image) { 
       let details = JSON.stringify(item.extendedIngredients);
-      let analyze = JSON.stringify(item.analyzedInstructions);
-      
-      //Pull name
-
-      // var getIng = details.map(function (ingNew) {
-      //   return ingNew.name;  
-      // });
-      
-      // ES6
-      let getIng = details.map(ingNew => {
-        return ingNew.name;
-      });
-      
-      console.log(getIng);
+      let analyze = JSON.stringify(details.name);
+      console.log(analyze);
+      console.log(details[name]);
 
       const recipe = `
         <div class="card" style="width: 18rem;">
           <h5 class="card-title" id="recipeName">${item.title}</h5>
           <img class="card-img-top"id="image" src="${image}" alt="Card image cap" />
-          <button id="mealPick" type="button" class="btn btn-warning">PICK THIS RECIPE</button>
+          <button data-recipe=${item.instructions} id="${item.id}" type="button" name="${item.id}" class="btn btn-warning ${item.id}">PICK THIS RECIPE</button>
           <div class="card-body">
             <p class="card-text" id="recipe">${item.summary}</p>
             <p class="card-text" id="recipe">${details}</p>
-            <p class="card-text" id="recipe">${analyze}</p>
           </div>
         </div>`
-        console.log(recipe);
+        // console.log(recipe);
       ;
       $('#recipeOp').append(recipe);
-
-      //MAKES RECIPE CHOICE DIV
-
-      $('#mealPick').one("click", function() {
-        console.log('New Card');
-
-        $('#recipeOp').empty();
-          
-        const recipeChoice = `
-        <div class="card" style="width: 18rem;">
-          <h5 class="card-title" id="recipeName">${item.title}</h5>
-          <img class="card-img-top"id="image" src="${image}" alt="Card image cap" />
-          <div class="card-body">
-          <p class="card-text" id="recipe">${details}</p>
-            <p class="card-text" id="instruct">${item.instructions}</p>
-          </div>
-        </div>`;
-        console.log(recipeChoice)
-        $('#recipeOp').append(recipeChoice);
-      });
     }
     $('#recipeOp').empty();
-    
     getFood();
-    
   });
-  
 });
+
+  $("button[name='${item.id}']").on("click", function(){
+  //MAKES RECIPE CHOICE DIV
+    console.log('New Card');
+    $('#recipeOp').empty();
+
+    const recipeChoice = `
+      <div class="card" style="width: 18rem;">
+        <h5 class="card-title" id="recipeName">${item.title}</h5>
+        <img class="card-img-top"id="image" src="${image}" alt="Card image cap" />
+          <div class="card-body">
+            <p class="card-text" id="recipe">${details}</p>
+            <p class="card-text" id="instruct">${item.instructions}</p>
+          </div>
+      </div>`;
+
+          console.log(recipeChoice)
+
+          $('#recipeOp').append(recipeChoice);
+  });
+
+
 
 function loading (){
   console.log("loading")
@@ -88,38 +76,3 @@ function removeLoading () {
   console.log("loading removed")
   $(".loading").remove();
 }
-
-// $('#mealPick').click(function() {
-//   console.log('New Card');
-//   // $('#recipeOp').empty();
-//   // const recipe = `
-//   // <div class="card" style="width: 18rem;">
-//   //   <h5 class="card-title" id="recipeName">${item.title}</h5>
-//   //   <img class="card-img-top"id="image" src="${image}" alt="Card image cap" />
-//   //   <div class="card-body">
-//   //     <p class="card-text" id="instruct"></p>
-//   //     <p class="card-text" id="recipe">${item.summary}</p>
-//   //   </div>
-//   //   <button id="mealPick" type="button" class="btn btn-warning">PICK THIS RECIPE</button>
-//   // </div>`;
-// });
-
-
-
-
-
-      
-        //     function ButtonClicked() {
-        //         $('#formsubmitbutton').hide();
-        //         $('#formElem').hide();
-        //         $('#buttonreplacement').show(); 
-        //         $('#buttonreplacement').append(
-        //             `<h2>YOUR HERO</h2>
-        //             <img src="img/sVen.jpg" alt="loading...">
-        //             <p>Weapon Choice: ${$('#weaponChoiceH option:selected').text()}</p>
-        //             <h1>VS.</h1>
-        //             <h2>YOUR VILLIAN</h2>
-        //             <img src="img/dragon-legendary-creature-sea-serpent-art-creatures-png-clip-art.png" alt="loading...">
-        //             <p>Weapon Choice: ${$('#weaponChoiceV option:selected').text()}</p>`)
-        
-        //     };
