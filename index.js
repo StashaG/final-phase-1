666a51bb8fbce5ead11d40a7569cf924694e4960
Robert <r.powelliv@gmail.com>
2020-09-10 22:19:14 -0400

    RECIPE PICK FINISH

--- index.js
+++ index.js
@@ -21,10 +21,17 @@ $(()=> {
     }
     
     function getRecipe(item, image) { 
-      let details = JSON.stringify(item.extendedIngredients);
-      let analyze = JSON.stringify(details.name);
-      console.log(analyze);
-      console.log(details[name]);
+      let details = item.extendedIngredients;
+      let getIng = details.map(ingNew => {
+        return ingNew.name;
+      });
+        console.log(getIng);
+
+      let getAmount = details.map(ingAmt => {
+          return ingAmt.original;
+        });
+          console.log(getAmount);
+    
 
       const recipe = `
         <div class="card" style="width: 18rem;">
@@ -35,35 +42,61 @@ $(()=> {
             <p class="card-text" id="recipe">${item.summary}</p>
             <p class="card-text" id="recipe">${details}</p>
           </div>
-        </div>`
-        // console.log(recipe);
-      ;
+        </div>`;
       $('#recipeOp').append(recipe);
+
+    $(`#${item.id}`).on("click", function() {
+      console.log('New Card');
+      $('#recipeOp').empty();
+          
+      $(function () {
+        $("#tabs").tabs();
+      });
+
+        const recipeChoice =`
+            <div id= "tabs">
+                <ul>
+                 <li><a href="#tabs-1">Meal</a></li>
+                 <li><a href="#tabs-2">Ingredients</a></li>
+                 <li><a href="#tabs-3">Directions</a></li>
+               </ul>
+               <div id="tabs-1">
+                 <h3>${item.title}</h3>
+                 <img src="${image}" alt="Meal Image">
+                 
+               </div>
+               <div id ="tabs-2">   
+                <h5>Ingredients:</h5>
+                <ul>
+                ${getAmount
+                  .map((ingredient) => `<li>${ingredient}</li>`)
+                  .join("")}
+
+                </ul>
+               </div>
+               <div id="tabs-3">
+                <h4>${item.title}</h4>
+                <p>${item.instructions}</p>
+                </div>`;
+    
+    
+              console.log(recipeChoice)
+    
+              $('#recipeOp').append(recipeChoice);
+      });
     }
     $('#recipeOp').empty();
     getFood();
   });
 });
 
-  $("button[name='${item.id}']").on("click", function(){
-  //MAKES RECIPE CHOICE DIV
-    console.log('New Card');
-    $('#recipeOp').empty();
 
-    const recipeChoice = `
-      <div class="card" style="width: 18rem;">
-        <h5 class="card-title" id="recipeName">${item.title}</h5>
-        <img class="card-img-top"id="image" src="${image}" alt="Card image cap" />
-          <div class="card-body">
-            <p class="card-text" id="recipe">${details}</p>
-            <p class="card-text" id="instruct">${item.instructions}</p>
-          </div>
-      </div>`;
+  //     $(function () {
+  //       $("#tabs").tabs();
+  //     });
+    
 
-          console.log(recipeChoice)
-
-          $('#recipeOp').append(recipeChoice);
-  });
+  // });
 
 
 
